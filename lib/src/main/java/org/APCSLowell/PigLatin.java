bb318bf8e2aac2153205ccefe6bae21c22fee073
breathaning <etzhu1@gmail.com>
2026-01-20 21:20:34 -0800

    Completed assignment. I added cases for vowels, Q's, consonants, and the rest. I double checked with all of the test cases.

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -3,7 +3,7 @@ package org.APCSLowell;
 public class PigLatin {
     public int findFirstVowel(String sWord) {
         //precondition: sWord is a valid String of length greater than 0.
-        if (sWord.length() <= 0) { return false; }
+        if (sWord.length() <= 0) { return -1; }
         //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
 	    for (int i = 0; i < sWord.length(); i++) {
             char letter = sWord.charAt(i);
@@ -16,14 +16,19 @@ public class PigLatin {
 
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
-        if (sWord.length() <= 0) { return false; }
+        if (sWord.length() <= 0) { return null; }
         //postcondition: returns the pig latin equivalent of sWord
         int vowelPosition = findFirstVowel(sWord);
-        if (firstVowel == -1) {
-            return sWord + "ay";
+        String rearrangedWord;
+        if (vowelPosition == -1) {
+            rearrangedWord = sWord;
+        } else if (vowelPosition == 0) {
+            rearrangedWord = sWord + "w";
+        } else if (sWord.charAt(0) == 'q') {
+            rearrangedWord = sWord.substring(2, sWord.length()) + sWord.substring(0, 2);
         } else {
-            return sWord.substring()
+            rearrangedWord = sWord.substring(vowelPosition, sWord.length()) + sWord.substring(0, vowelPosition);
         }
-        return null;
+        return rearrangedWord + "ay";
     }
 }
